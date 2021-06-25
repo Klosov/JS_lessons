d1abb8a8053bbeb0201ee62dbcf59a758a9aa9af
Кльосов Юрій Ігорович <klosov@oree.com.ua>
2021-06-25 20:36:37 +0300

    Classwork & Homework #3

--- L3/HW_3/1.js
+++ L3/HW_3/1.js
@@ -1,5 +1,4 @@
 /*
-
   Задание написать простой слайдер.
 
     Есть массив с картинками из которых должен состоять наш слайдер.
@@ -15,8 +14,47 @@
     4. PrevSlide -> Тоже самое что предыдущий вариант, но на кнопку PrevSlide и передавать currentPosition - 1
       + бонус сделать так что бы при достижении последнего и первого слада вас после кидало на первый и последний слайд соответственно.
       + бонсу анимация появления слайдов через навешивание дополнительных стилей
-
 */
 
-  var OurSliderImages = ['images/cat1.jpg', 'images/cat2.jpg', 'images/cat3.jpg', 'images/cat4.jpg', 'images/cat5.jpg', 'images/cat6.jpg', 'images/cat7.jpg', 'images/cat8.jpg'];
-  var currentPosition = 0;
+const srcImages = ['images/cat1.jpg', 'images/cat2.jpg', 'images/cat3.jpg', 'images/cat4.jpg', 'images/cat5.jpg', 'images/cat6.jpg', 'images/cat7.jpg', 'images/cat8.jpg'],
+      NextSlide = document.getElementById('NextSlide'),
+      PrevSlide = document.getElementById('PrevSlide'),
+      slider = document.getElementById('slider'),
+      img = document.createElement('img');
+let currentPosition = 0;
+
+img.src = srcImages[currentPosition];
+setTimeout( () => {img.classList.add('show')}, 50);
+
+RenderImage = function () {
+  slider.innerHTML = '';
+  img.classList.remove('show');
+
+  if (currentPosition === srcImages.length)
+    currentPosition = 0;
+  else if (currentPosition < 0)
+    currentPosition = srcImages.length - 1;
+
+  img.src = srcImages[currentPosition];
+
+  slider.appendChild(img);
+
+  setTimeout( () => {img.classList.add('show');}, 50);
+}
+
+prevSlide = () => {
+  currentPosition--;
+  RenderImage();
+}
+
+nextSlide = () => {
+  currentPosition++;
+  RenderImage();
+}
+
+window.addEventListener('load', function () {
+  slider.appendChild(img);
+
+  PrevSlide.addEventListener('click', prevSlide);
+  NextSlide.addEventListener('click', nextSlide);
+});
