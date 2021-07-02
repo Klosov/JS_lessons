d85bbe9fb7660fc7b60057ee8c69d2bbcec9df4c
Кльосов Юрій Ігорович <klosov@oree.com.ua>
2021-07-02 16:43:26 +0300

    Homework #5

--- L5/HW_5/task1.js
+++ L5/HW_5/task1.js
@@ -1,5 +1,4 @@
 /*
-
   Задание:
 
     1. Написать конструктор объекта комментария который принимает 3 аргумента
@@ -23,6 +22,44 @@
       И выводит каждый из них на страничку.
 
     <div id="CommentsFeed"></div>
-
-
 */
+
+function Comment(name, msg, avatarUrl) {
+    this.name = name;
+    this.msg = msg;
+    this.avatarUrl = avatarUrl;
+    this.likes = 0;
+}
+
+function ShowComments(arrComments) {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'CommentsFeed');
+
+    document.body.appendChild(div);
+
+    arrComments.forEach((value) => {
+        let commentDiv = document.createElement('div');
+        commentDiv.innerHTML = 'Name: ' + value.name + ' Message: ' + value.msg + '<img src="' + value.avatarUrl + '" alt="avatar" style="width:75px">';
+
+        div.appendChild(commentDiv);
+    })
+}
+
+const defaultSettings = {
+    avatarUrl: 'avatar_default.jpg',
+    increaseLikes: () => {
+        return this.likes++;
+    }
+}
+
+let myComment1 = new Comment('Mike', 'Cool video!', 'avatar.jpg');
+let myComment2 = new Comment('Nick', 'The best!', 'avatar.jpg');
+let myComment3 = new Comment('Petter', 'Perfecto', 'avatar_default.jpg');
+let myComment4 = new Comment('Harry', 'Avada kedavra!', 'avatar.jpg');
+
+const CommentsArray = [myComment1, myComment2, myComment3, myComment4];
+
+/*let newMyCall = Comment.bind(defaultSettings);
+newMyCall();*/
+
+const test = new ShowComments(CommentsArray);
