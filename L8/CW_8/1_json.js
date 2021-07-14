c5deb7ebfda8c4dbba8202fb9bdf425d0cee09ad
Кльосов Юрій Ігорович <klosov@oree.com.ua>
2021-07-14 21:03:26 +0300

    L8

---
+++ L8/CW_8/1_json.js
@@ -0,0 +1,34 @@
+
+/*
+  Задание:
+  Написать скрипт который:
+    1. Собирает данные с формы (3 разных полей), конвертирует их в json и выводит в консоль.
+  ->  2. Сделать отдельный инпут который выполняет JSON.parse(); на ту строку что вы туда ввели и выводит результат в консоль.
+
+  Array.from(HTMLNodeColection); -> Arary
+
+  <form>
+    <input name="name" />
+    <input name="age"/>
+    <input name="password"/>
+    <button></button>
+  </form>
+
+  <form>
+    <input />
+    <button></button>
+  </form>
+  -> '{"name" : !"23123", "age": 15, "password": "*****" }'
+
+*/
+
+
+const formData = new FormData();
+formData.append('name', 'Tester');
+formData.append('city', 'Test');
+formData.append('age', '20');
+
+// Display the values
+for (var value of formData.values()) {
+    console.log(value);
+}
