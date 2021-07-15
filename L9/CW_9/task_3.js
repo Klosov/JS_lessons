97ef6bfee8f1c6e5901d81e35d73f39d9648f2f5
Кльосов Юрій Ігорович <klosov@oree.com.ua>
2021-07-15 22:39:57 +0300

    L9 CW_3

--- L9/CW_9/task_3.js
+++ L9/CW_9/task_3.js
@@ -1,7 +1,5 @@
 /*
-
     Задание 3:
-
 
     Написать класс Posts в котором есть данные:
 
@@ -18,5 +16,51 @@
     - После добавления поста, данные о нем записываются в localStorage.
     - После перезагрузки страницы, данные должны сохраниться.
     - Можно было предзагрузить данные в класс из апи: http://www.json-generator.com/api/json/get/cgCRXqNTtu?indent=2
-
 */
+
+const posts = document.getElementById('posts');
+
+class Posts {
+    likes = 0;
+
+    constructor(_id, isActive, title, about, created_at) {
+        this._id = _id;
+        this.isActive = isActive;
+        this.title = title;
+        this.about = about;
+        this.created_at = created_at;
+    }
+
+    addLike(id) {
+        this.likes++
+        console.log('Likes: ' + this.likes);
+    }
+
+    render() {
+        return `<div style="width:250px;">
+                    <h2>${this.title}</h2>
+                    <small>${this.created_at}</small>
+                    <small>${this.isActive}</small>
+                    <p>${this.about}</p>
+                    <input type="hidden" name="_id" value="${this._id}">
+                </div>`;
+    }
+}
+
+
+
+(function getData() {
+    fetch('https://www.json-generator.com/api/json/get/cgCRXqNTtu?indent=2')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let html = '';
+            data.forEach( (post) => {
+                window[post._id] = new Posts(post._id, post.isActive, post.title, post.about, post.created_at);
+                html += window[post._id].render();
+            });
+
+            posts.innerHTML = html;
+        });
+})();
