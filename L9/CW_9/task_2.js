946c073803abf75f8d72de3e7c05ef1457b44b7c
Кльосов Юрій Ігорович <klosov@oree.com.ua>
2021-07-15 21:52:44 +0300

    L9 CW_2

--- L9/CW_9/task_2.js
+++ L9/CW_9/task_2.js
@@ -1,5 +1,4 @@
 /*
-
     Задание 2:
     Написать форму логина (логин пароль), которая после отправки данных записывает их в localStorage.
     Если в localStorage есть записть - На страниче вывести "Привет {username}", если нет - вывести окно
@@ -8,6 +7,38 @@
     + бонус, сделать кнопку "выйти" которая удаляет запись из localStorage и снова показывает форму логина
     + бонус сделать проверку логина и пароля на конкретную запись. Т.е. залогинит пользователя если
     он введет только admin@example.com и пароль 12345678
-
-
 */
+const form = document.getElementById('loginForm'),
+    logoutBtn = document.getElementById('logout'),
+    loginData = localStorage.getItem("loginData");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (form.email.value === 'admin@example.com' && form.password.value === '12345678') {
+        let data = {
+            name: form.email.value,
+            age: form.password.value
+        }
+
+        let jsonData = JSON.stringify(data);
+
+        localStorage.setItem('loginData', jsonData);
+        location.reload();
+    } else {
+        alert('Wrong password or Email!');
+        return false;
+    }
+});
+
+logoutBtn.addEventListener('click', () => {
+    localStorage.removeItem("loginData");
+
+    location.reload();
+});
+
+if (loginData) {
+    form.style.display = 'none';
+    document.querySelector('h1').innerText = `Привет, ${JSON.parse(loginData).name}!`;
+}
+
