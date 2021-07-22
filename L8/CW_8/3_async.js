6540b7a9a6da9a64692a3326891c6667d88f0faa
Кльосов Юрій Ігорович <klosov@oree.com.ua>
2021-07-22 20:36:00 +0300

    CW 10

--- L8/CW_8/3_async.js
+++ L8/CW_8/3_async.js
@@ -1,7 +1,5 @@
 /*
-
   Задание:
-
     Написать при помощи async-await скрипт, который:
 
     Получает список компаний:  http://www.json-generator.com/api/json/get/ceRHciXcVu?indent=2
@@ -14,5 +12,9 @@
     4.| CompName | 2000$   | button                    | button
 
     Данные о дате регистрации и адресс скрывать при выводе и показывать при клике.
-
 */
+
+async function getData() {
+    
+}
+
